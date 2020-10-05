cefd4f0ef814f238f9b81f725278fa925e5adf2d
Vladius25 <vkorol2509@icloud.com>
2020-10-05 22:51:46 +0500

    Add jwt

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -45,6 +45,17 @@ MIDDLEWARE = [
     'simple_history.middleware.HistoryRequestMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
 ROOT_URLCONF = 'task_manager.urls'
 
 TEMPLATES = [
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
+from rest_framework_jwt.views import obtain_jwt_token
 
 api_urlpatterns = [
     path("tasks/", include("tasks.urls")),
@@ -9,4 +10,5 @@ urlpatterns = [
     re_path(r'^api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path("api/v1/", include(api_urlpatterns)),
+    path("login/", obtain_jwt_token),
 ]
