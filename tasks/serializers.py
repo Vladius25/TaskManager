6aa7b57f2ddde6954a8802b85583a1b4eec1b1eb
Vladius25 <vkorol2509@icloud.com>
2020-10-05 19:36:51 +0500

    Update serializers

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,29 +1,18 @@
 from rest_framework import serializers
 
-from tasks.models import Task, Status
-
-
-class StatusSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Status
-        fields = ("name", )
+from tasks.models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
-    status = serializers.SerializerMethodField()
-
-    def get_status(self, obj: Task):
-        return obj.status.name
-
     class Meta:
         model = Task
         fields = "__all__"
 
 
-class HistoricalTaskSerializer(TaskSerializer):
+class HistoricalTaskSerializer(serializers.ModelSerializer):
     update_date = serializers.SerializerMethodField()
 
-    def get_update_date(self, obj: Task):
+    def get_update_date(self, obj):
         return obj.history_date
 
     class Meta:
