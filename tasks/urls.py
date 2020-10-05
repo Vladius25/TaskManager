ac2ac3572053e54d9561ff2220ea36ff54bdd893
Vladius25 <vkorol2509@icloud.com>
2020-10-05 19:37:18 +0500

    Add get/create/update views

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from tasks.views import GetHistoryView
+from tasks.views import GetHistoryView, GetCreateTasksView, GetUpdateDeleteTaskView
 
 urlpatterns = [
-    path("history/<int:pk>", GetHistoryView.as_view(), name="get-history"),
+    path("", GetCreateTasksView.as_view()),
+    path("<int:pk>", GetUpdateDeleteTaskView.as_view()),
+    path("history/<int:pk>", GetHistoryView.as_view()),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,12 +2,29 @@ from rest_framework import generics
 from rest_framework.response import Response
 
 from tasks.models import Task
-from tasks.serializers import TaskSerializer, HistoricalTaskSerializer
+from tasks.serializers import HistoricalTaskSerializer, TaskSerializer
+
+
+class GetCreateTasksView(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        queryset = Task.objects.all()
+        query_params = self.request.query_params.dict()
+        conditions = {"status": "status", "from_date": "end_date__gte", "to_date": "end_date__lte"}
+        for param_name in conditions.keys():
+            if param_name in query_params:
+                queryset = queryset.filter(**{conditions[param_name]: query_params[param_name]})
+        return queryset
+
+
+class GetUpdateDeleteTaskView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
 
 class GetHistoryView(generics.RetrieveAPIView):
     queryset = Task.objects.all()
-    serializer_class = TaskSerializer
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
